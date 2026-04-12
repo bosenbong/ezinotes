8e9cf8274b9e6654ebffc30217297b896c5c81d8
Rod <rod@ezinotes.app>
2026-04-12 02:57:56 +0000

    Fix build - only compile index.html

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: './index.html',
-        game: './game.html',
-        rage: './rage.html',
-        fever: './fever.html',
-        fever3d: './fever3d.html',
-        feverBuild: './fever-build.html'
-      }
+      input: './index.html'
     }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173
   }
 })
